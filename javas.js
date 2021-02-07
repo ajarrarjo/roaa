a5034b5783c440b178a83ab61761cfe4e498a9a0
Abdullah Bilal Jarrar <ajarrarjo@yahoo.com>
2021-02-07 23:30:26 +0200

    JS

---
+++ javas.js
@@ -0,0 +1,12 @@
+var gender= prompt("What's your Gender?"); 
+
+if (gender === "male") { 
+    alert('male');
+};
+if(gender === "female") { 
+    alert('female')
+}
+var x= prompt("What's your Name?"); 
+alert (`hi ${x}`);
+var age = prompt('what is your age');
+alert (`العمر كلو`)
